956a414ab8a79507f78d55d2f0df425b52314ea6
cmpbad <cmpbad@twitch.tv>
2022-02-09 18:12:21 -0800

    Update test fixtures

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,4 +3,4 @@ flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./*.py
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./uuid_module/*.py
 
 # Ignored depreciation warnings for libraries outside our control
-pytest -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv
+pytest -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv | tee ./logs/pytest.log
