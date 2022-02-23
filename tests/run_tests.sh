52c50e5b9cace1c9a40757da0e2b48e5336cd86c
cmpbad <cmpbad@twitch.tv>
2022-02-23 12:28:52 -0800

    Return pages to Public, update local test script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,6 @@
+# Rebuild Docs
+pdoc -o docs/ -d google uuid_module
+
 # Flake tests
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./*.py
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./uuid_module/*.py
