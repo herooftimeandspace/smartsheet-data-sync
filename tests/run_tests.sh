047ad1c242c8d2d739e27548d2dcff568bcd3442
cmpbad <cmpbad@twitch.tv>
2022-03-15 14:14:35 -0700

    Refresh static test data

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,4 +6,4 @@ flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./*.py
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./uuid_module/*.py
 
 # Ignored depreciation warnings for libraries outside our control
-pytest --debug -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv | tee ./logs/pytest.log
+pytest --debug -x -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv | tee ./logs/pytest.log
