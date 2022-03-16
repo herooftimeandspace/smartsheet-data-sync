e694ff9194331011dd5f1c2a1f59ea4f0dcf8bfd
cmpbad <cmpbad@twitch.tv>
2022-03-16 04:09:27 -0700

    Config toggle for lite/full tests

--- test_fixtures/run_tests.sh
+++ test_fixtures/run_tests.sh
@@ -9,4 +9,4 @@ flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./test_integra
 
 # Ignored depreciation warnings for libraries outside our control
 pytest test_unit/ --debug -x -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv | tee ./logs/pytest_unit.log
-# pytest test_unit/ --debug -x -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv | tee ./logs/pytest_integration.log
+# pytest test_unit/ test_integration/ --debug -x -W ignore::DeprecationWarning --cov-config=.coveragerc --cov=./ -vvv | tee ./logs/pytest_integration.log
