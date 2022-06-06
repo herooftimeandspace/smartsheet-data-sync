6a43ff63b43f496b228934a7634629c6b0133ff7
cmpbad <cmpbad@twitch.tv>
2022-06-06 15:05:05 -0700

    update test script for more modules

--- test_fixtures/run_tests.sh
+++ test_fixtures/run_tests.sh
@@ -1,9 +1,12 @@
 # Rebuild Docs
-pdoc -o docs/ -d google uuid_module
+pdoc -o docs/ -d google uuid_module data_module sync_module app
 
 # Flake tests
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./*.py
+flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./app/*.py
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./uuid_module/*.py
+flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./data_module/*.py
+flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./sync_module/*.py
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./test_unit/*.py
 flake8 --max-line-length=80 --per-file-ignores="__init__.py:F401" ./test_integration/*.py
 
